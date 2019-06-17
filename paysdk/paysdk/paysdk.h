446137e5f6a380c913d729e83899f077fdb91979
lee <876231865@qq.com>
2019-06-17 17:02:29 +0800

    修复

---
+++ paysdk/paysdk/paysdk.h
@@ -0,0 +1,19 @@
+//
+//  paysdk.h
+//  paysdk
+//
+//  Created by lee on 2019/6/17.
+//  Copyright © 2019 xqc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for paysdk.
+FOUNDATION_EXPORT double paysdkVersionNumber;
+
+//! Project version string for paysdk.
+FOUNDATION_EXPORT const unsigned char paysdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <paysdk/PublicHeader.h>
+
+
---
+++ paysdk/paysdkdemo/paysdkdemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  paysdkDemo
+//
+//  Created by lee on 2019/6/17.
+//  Copyright © 2019 xqc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
